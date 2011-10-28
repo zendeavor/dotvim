8fbbe2fcd1a5215012c7072120db6a53ecde4896
Josh McGee <j.s.mcgee115@gmail.com>
2011-10-28 00:14:57 -0700

    initial commit

---
+++ bundle/.clones.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/Shougo/neocomplcache.git
+git clone https://github.com/tpope/vim-repeat.git
+hg clone https://hg@bitbucket.org/ns9tks/vim-l9
+git clone https://github.com/tpope/vim-unimpaired.git
+hg clone https://hg@bitbucket.org/ns9tks/vim-fuzzyfinder
+git clone https://github.com/mbadran/headlights.git
+git clone https://github.com/mileszs/ack.vim.git
+

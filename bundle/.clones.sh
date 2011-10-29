3f90a87e600f6036a4eb59ea519dd9a48382601a
Josh McGee <j.s.mcgee115@gmail.com>
2011-10-29 01:28:41 -0700

    removed git clones

--- bundle/.clones.sh
+++ bundle/.clones.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/Shougo/neocomplcache.git
-git clone https://github.com/tpope/vim-repeat.git
 hg clone https://hg@bitbucket.org/ns9tks/vim-l9
-git clone https://github.com/tpope/vim-unimpaired.git
 hg clone https://hg@bitbucket.org/ns9tks/vim-fuzzyfinder
-git clone https://github.com/mbadran/headlights.git
-git clone https://github.com/mileszs/ack.vim.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/klen/python-mode.git
-git clone https://github.com/tomtom/stakeholders_vim.git
-git clone http://github.com/sjl/gundo.vim.git
-git clone https://github.com/godlygeek/tabular.git
 

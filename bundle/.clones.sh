f379d477967c7ec6b086069bccae89ffcf96e0d0
Josh McGee <j.s.mcgee115@gmail.com>
2011-10-29 01:19:13 -0700

    made submodules for easier tracking

--- bundle/.clones.sh
+++ bundle/.clones.sh
@@ -7,4 +7,9 @@ git clone https://github.com/tpope/vim-unimpaired.git
 hg clone https://hg@bitbucket.org/ns9tks/vim-fuzzyfinder
 git clone https://github.com/mbadran/headlights.git
 git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/klen/python-mode.git
+git clone https://github.com/tomtom/stakeholders_vim.git
+git clone http://github.com/sjl/gundo.vim.git
+git clone https://github.com/godlygeek/tabular.git
 
